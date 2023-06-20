97b84a8c5096a6a53abba1a24c0319538e6f62f4
Shamil <shamilvkage@gmail.com>
2023-06-20 12:45:52 +0530

    new update

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Do You Want To Be a Developer? \n' 
+    );
+
+    await sleep();
+    rainbowTitle.stop();
+
+    console.log(`
+        ${chalk.bgGreenBrigh('HOW TO START')}
+        I am but a conscience.
+        If you get any tasks wrong I will be ${chalk.bgRedBright('Killed')}
+        so get all of them Right...
+
+    `);
+}
+
+await welcome()
