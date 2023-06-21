1e714dfbc5aaf927d96a4b43e92cbf07a66d9993
Shamil <shamilvkage@gmail.com>
2023-06-21 10:50:01 +0530

    final update

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ async function welcome() {
     `);
 }
 
+async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Checking your Answer...').start();
+    await sleep();
+
+    if (isCorrect) {
+        spinner.success({ 
+            text: `Nice work ${playerName}. That's the legit one.` 
+        });
+    } else {
+        spinner.error({ 
+            text: `GAME OVER mwonuse...., you have failed ${playerName}!`
+         });
+         process.exit(1);
+    }
+}
+
 async function askname() {
     const answers = await inquirer.prompt({
         name: 'player_name',
@@ -55,21 +71,55 @@ async function question1() {
     return handleAnswer(answer.question_1);
 }
 
-async function handleAnswer(isCorrect) {
-    const spinner = createSpinner('Checking your Answer...').start();
-    await sleep();
-
-    if (isCorrect) {
-        spinner.success({ 
-            text: `Nice work ${playerName}. That's the legit one.` 
-        });
-    } else {
-        spinner.error({ 
-            text: `GAME OVER mwonuse...., you have failed ${playerName}!`
-         });
-         process.exit(1);
-    }
-}
+async function question2() {
+    const answers = await inquirer.prompt({
+      name: 'question_2',
+      type: 'list',
+      message: 'What is x? var x = 1_1 + "1" + Number(1)\n',
+      choices: ['4', '"4"', '"1111"', '69420'],
+    });
+    return handleAnswer(answers.question_2 === '"1111"');
+  }
+  
+  async function question3() {
+    const answers = await inquirer.prompt({
+      name: 'question_3',
+      type: 'list',
+      message: `What is the first element in the array? ['🐏', '🦙', '🐍'].length = 0\n`,
+      choices: ['0', '🐏', '🐍', 'undefined'],
+    });
+  
+    return handleAnswer(answers.question_3 === 'undefined');
+  }
+  
+  async function question4() {
+    const answers = await inquirer.prompt({
+      name: 'question_4',
+      type: 'list',
+      message: 'Which of the following is NOT a primitive type?\n',
+      choices: [
+        'boolean',
+        'number',
+        'null',
+        'object', // Correct
+      ],
+    });
+    return handleAnswer(answers.question_4 === 'object');
+  }
+  
+  async function question5() {
+    const answers = await inquirer.prompt({
+      name: 'question_5',
+      type: 'list',
+      message:
+        'JS is a high-level single-threaded, garbage-collected,\n' +
+        'interpreted(or just-in-time compiled), prototype-based,\n' +
+        'multi-paradigm, dynamic language with a ____ event loop\n',
+      choices: ['multi-threaded', 'non-blocking', 'synchronous', 'promise-based'],
+    });
+  
+    return handleAnswer(answers.question_5 === 'non-blocking');
+  }
 
 function winner() {
     console.clear();
@@ -80,8 +130,12 @@ function winner() {
     });
 }
 
-
-// await welcome()
-// await askname()
-// await question1();
-await winner()
+console.clear();
+await welcome()
+await askname()
+await question1();
+await question2();
+await question3();
+await question4();
+await question5();
+winner()
